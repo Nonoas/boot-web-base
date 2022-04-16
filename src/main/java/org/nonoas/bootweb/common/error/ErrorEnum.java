3061f89e7e331f5a6c600951c967ff0f40f00e83
Nonoas <1356955215@qq.com>
2022-04-16 14:02:10 +0800

    完善错误枚举

--- src/main/java/org/nonoas/bootweb/common/error/ErrorEnum.java
+++ src/main/java/org/nonoas/bootweb/common/error/ErrorEnum.java
@@ -14,14 +14,45 @@ public enum ErrorEnum implements IErrorReturnType {
     UNKNOWN_ERROR("00001", "未知错误"),
 
     /**
+     * A0001：数据不存在
+     */
+    DATA_NOT_FOUND("A0001", "数据不存在"),
+
+    /**
      * A0111：用户已存在
      */
     USER_EXISTS("A0111", "用户已存在"),
 
     /**
+     * A0210：账号或密码错误
+     */
+    LOGIN_FAIL("A0210", "用户名或密码错误"),
+
+    /**
+     * A0230：用户未登录或登录已过期
+     */
+    NO_LOGIN("A0230", "用户未登录"),
+
+    /**
+     * A0300：访问权限异常
+     */
+    AUTH_ERROR("A0300", "访问权限异常"),
+
+
+    /**
+     * A0400: 请求参数宏观错误
+     */
+    BAD_PARAM("A0400", "用户请求参数错误"),
+
+    /**
      * A0402：请求参数值超出允许的范围
      */
-    BAD_PARAM_VALUE("A0402", "请求参数值超出允许的范围"),
+    PARAM_OUT_OF_RANGE("A0402", "请求参数值超出允许的范围"),
+
+    /**
+     * A0421：参数格式不匹配
+     */
+    PARAM_FORMAT_MISMATCH("A0421", "参数格式不匹配"),
 
     /**
      * A0501：访问频率过高
@@ -31,7 +62,12 @@ public enum ErrorEnum implements IErrorReturnType {
     /**
      * A0506：用户重复请求
      */
-    REPEAT_REQUEST("A0506", "用户重复请求");
+    REPEAT_REQUEST("A0506", "用户重复请求"),
+
+    /**
+     * A0700: 用户上传文件异常
+     */
+    UPLOAD_ERROR("A0700", "用户上传文件异常");
 
 
     private final String errorCode;
